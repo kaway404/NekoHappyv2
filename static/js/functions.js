b401803e38651f5ad0c9e2a4729c8ea5acb93ace
kaway404 <32229419+kaway404@users.noreply.github.com>
2018-01-21 18:35:18 -0200

    Fix login and register

---
+++ static/js/functions.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $("#registrar").click(function() {
+        var emailr = $("#emailr").val(); 
+        var senhar = $("#senhar").val();
+		var nomer = $("#nomer").val(); 
+        var sobrenomer = $("#sobrer").val();
+        var pincode = $("#pincode").val();
+        $.post("register.php", {emailr: emailr,senhar: senhar,nomer: nomer,sobrenomer: sobrenomer,pincode: pincode},
+        function(data){
+         $("#resposta").html(data);
+         }
+         , "html"); 
+         return false;
+    });
+});
+
+$(document).ready(function() {
+    $("#logar").click(function() {
+        var emaill = $("#emaill").val(); 
+        var senhal = $("#senhal").val();
+        $.post("login.php", {emaill: emaill,senhal: senhal},
+        function(data){
+         $("#resposta").html(data);
+         }
+         , "html");
+         return false;
+    });
+});
