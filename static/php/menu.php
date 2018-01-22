201901d3e4c8db69db4de35f2dfc4bb06cd3e38f
Alexandre Silva <xande1231231@hotmail.com>
2018-01-22 20:43:09 -0200

    Fixed post fixed

--- static/php/menu.php
+++ static/php/menu.php
@@ -70,35 +70,73 @@ else
                                                     echo $msg;
                                                     ?></p>
 
-                                                    <div class="post-bottom">
-	<div class="feed">
-	 <a class="like-btn">
-      <div class="reaction-box">
-        <div id="gosteireact" class="reaction-icon">
-        <img src="/static/img/react/like.gif" class="react-icon-b-gif">
-         <label>Gostei</label>
-        </div>
-        <div class="reaction-icon>
-        	<img src=/static/img/react/amei.gif" class="react-icon-b-gif">
-         <label>Amei</label>
-        </div>
-        <div class="reaction-icon">
-        	<img src="/static/img/react/odeio.gif" class="react-icon-b-gif">
-         <label>Odeio</label>
-        </div>
-        <div class="reaction-icon">
-        	<img src="/static/img/react/haha.gif" class="react-icon-b-gif">
-         <label>Haha</label>
-        </div>
-      </div>
-  </a>
-		</div>
-	</div>
+                                        <div class="post-bottom">
+  <div class="feed">
+<?php
+$comentid = $coment['id'];
+$iduser = DBEscape( strip_tags(trim($_COOKIE['iduser']) ) );
+$comentiduser = $coment['id'];
+$likes = DBRead( 'like', "WHERE idpost = $comentiduser and iduser = $iduser ORDER BY id DESC" );
+if (!$likes)
+echo '<a class="like-btn" id="kawaii'.$comentiduser.'">';
+else  
+  foreach ($likes as $like):
+?>
+   <a class="like-btn ativolike" id="kawaii<?php echo $comentiduser; ?>">
+   <?php endforeach;?>
+    </a>
 
-		</div>
+    </div>
+  </div>
 
 
-<?php endforeach; endforeach; ?>
+    </div>
+
+<script>
+var kawaii<?php echo $comentiduser; ?> = document.getElementById('kawaii<?php echo $comentiduser; ?>');
+
+</script>
+
+<?php
+$dbCheck = DBRead( 'like', "WHERE id and idpost = $comentiduser and iduser = $iduser" );
+
+if( $dbCheck ){
+?>
+<script>
+   $(document).ready(function() {
+    $("#kawaii<?php echo $comentiduser; ?>").click(function() {
+        var post = <?php echo $coment['id'] ?>; 
+        $.post("/static/php/react.php", {post: post},
+        function(data){
+         $("#respostaba").html(data);
+         }
+         , "html");
+         return false;
+    });
+});
+</script>
+<?php } else{ ?>
+<script>
+   $(document).ready(function() {
+    $("#kawaii<?php echo $comentiduser; ?>").click(function() {
+        var post = <?php echo $coment['id'] ?>; 
+        $.post("/static/php/react.php", {post: post},
+        function(data){
+         $("#respostaba").html(data);
+         }
+         , "html");
+         return false;
+    });
+});
+</script>
+<?Php } ?>
+
+
+<?php endforeach; endforeach;?>
+
+<div id="respostaba">
+
+</div>
 
 <h1 id="cont2">Publicaçãos normais.</h1>
 
