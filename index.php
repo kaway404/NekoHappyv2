5432963ee57d6ac1173f502b92c39e9f4756d7e3
Alexandre Silva <32229419+kaway404@users.noreply.github.com>
2018-03-11 20:29:07 -0300

    Fix CSS

--- index.php
+++ index.php
@@ -8,7 +8,6 @@ require 'static/php/system/config.php';
 	<title>NekoHappy</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="/static/css/style.css"/>
-	<link rel="stylesheet" type="text/css" href="/static/bootstrap/css/bootstrap.css"/>
 	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
 	<link rel="stylesheet" href="/static/css/react.css">
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
@@ -16,7 +15,6 @@ require 'static/php/system/config.php';
 	<script src="static/js/jquery.js" type="text/javascript"></script>
 	<script src="static/js/js-all.js" type="text/javascript"></script>
 	<script  src="static/js/index.js"></script>
-	<script  src="static/bootstrap/js/bootstrap.js"></script>
 </head>
 <body id="momer">
 <?php
