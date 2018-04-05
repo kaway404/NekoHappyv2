2a21f6ed94be57982a0f657d3c77fba1486a5854
Alexandre Silva <xande1231231@hotmail.com>
2018-04-05 17:29:13 -0300

    Fix

--- static/php/dashboard.php
+++ static/php/dashboard.php
@@ -1,7 +1,8 @@
 <?php
 if(isset($_COOKIE['iduser']) and (isset($_COOKIE['inisession'])) and (isset($_COOKIE['thecry']))){
   	$iduser = DBEscape( strip_tags(trim($_COOKIE['iduser']) ) );
-    $user = DBRead('user', "WHERE id = '{$iduser}' LIMIT 1 ");
+    $thecry = DBEscape( strip_tags( trim( $_COOKIE['thecry'] ) ) );
+    $user   = DBRead( 'user', "WHERE id = '{$iduser}' and thecry  = '{$thecry}' LIMIT 1" );
     $user = $user[0];
     $idcry = DBEscape( strip_tags(trim($_COOKIE['thecry']) ) );
     $usercry = DBRead('user', "WHERE thecry = '{$idcry}' LIMIT 1 ");
